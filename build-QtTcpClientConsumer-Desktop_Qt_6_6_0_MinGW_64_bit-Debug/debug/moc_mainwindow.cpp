d8438a1d2d9b788e41214115cbcd0fea30ae98da
polianaraujo <polianaellena123@gmail.com>
2023-07-16 17:58:26 -0300

    feat:dev

--- build-QtTcpClientConsumer-Desktop_Qt_6_6_0_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
+++ build-QtTcpClientConsumer-Desktop_Qt_6_6_0_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
@@ -39,53 +39,33 @@ namespace {
 struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {};
 static constexpr auto qt_meta_stringdata_CLASSMainWindowENDCLASS = QtMocHelpers::stringData(
     "MainWindow",
-    "timerEvent",
-    "",
-    "QTimerEvent*",
-    "event",
     "getData",
-    "setIpProd",
-    "showProd",
+    "",
     "startTemp",
     "stopTemp"
 );
 #else  // !QT_MOC_HAS_STRING_DATA
 struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {
-    uint offsetsAndSizes[20];
+    uint offsetsAndSizes[10];
     char stringdata0[11];
-    char stringdata1[11];
+    char stringdata1[8];
     char stringdata2[1];
-    char stringdata3[13];
-    char stringdata4[6];
-    char stringdata5[8];
-    char stringdata6[10];
-    char stringdata7[9];
-    char stringdata8[10];
-    char stringdata9[9];
+    char stringdata3[10];
+    char stringdata4[9];
 };
 #define QT_MOC_LITERAL(ofs, len) \
     uint(sizeof(qt_meta_stringdata_CLASSMainWindowENDCLASS_t::offsetsAndSizes) + ofs), len 
 Q_CONSTINIT static const qt_meta_stringdata_CLASSMainWindowENDCLASS_t qt_meta_stringdata_CLASSMainWindowENDCLASS = {
     {
         QT_MOC_LITERAL(0, 10),  // "MainWindow"
-        QT_MOC_LITERAL(11, 10),  // "timerEvent"
-        QT_MOC_LITERAL(22, 0),  // ""
-        QT_MOC_LITERAL(23, 12),  // "QTimerEvent*"
-        QT_MOC_LITERAL(36, 5),  // "event"
-        QT_MOC_LITERAL(42, 7),  // "getData"
-        QT_MOC_LITERAL(50, 9),  // "setIpProd"
-        QT_MOC_LITERAL(60, 8),  // "showProd"
-        QT_MOC_LITERAL(69, 9),  // "startTemp"
-        QT_MOC_LITERAL(79, 8)   // "stopTemp"
+        QT_MOC_LITERAL(11, 7),  // "getData"
+        QT_MOC_LITERAL(19, 0),  // ""
+        QT_MOC_LITERAL(20, 9),  // "startTemp"
+        QT_MOC_LITERAL(30, 8)   // "stopTemp"
     },
     "MainWindow",
-    "timerEvent",
-    "",
-    "QTimerEvent*",
-    "event",
     "getData",
-    "setIpProd",
-    "showProd",
+    "",
     "startTemp",
     "stopTemp"
 };
@@ -99,7 +79,7 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
       12,       // revision
        0,       // classname
        0,    0, // classinfo
-       6,   14, // methods
+       3,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -107,17 +87,11 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags, initial metatype offsets
-       1,    1,   50,    2, 0x0a,    1 /* Public */,
-       5,    0,   53,    2, 0x0a,    3 /* Public */,
-       6,    0,   54,    2, 0x0a,    4 /* Public */,
-       7,    0,   55,    2, 0x0a,    5 /* Public */,
-       8,    0,   56,    2, 0x0a,    6 /* Public */,
-       9,    0,   57,    2, 0x0a,    7 /* Public */,
+       1,    0,   32,    2, 0x0a,    1 /* Public */,
+       3,    0,   33,    2, 0x0a,    2 /* Public */,
+       4,    0,   34,    2, 0x0a,    3 /* Public */,
 
  // slots: parameters
-    QMetaType::Void, 0x80000000 | 3,    4,
-    QMetaType::Void,
-    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -134,14 +108,7 @@ Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
     qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSMainWindowENDCLASS_t,
         // Q_OBJECT / Q_GADGET
         QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>,
-        // method 'timerEvent'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        QtPrivate::TypeAndForceComplete<QTimerEvent *, std::false_type>,
         // method 'getData'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        // method 'setIpProd'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        // method 'showProd'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
         // method 'startTemp'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
@@ -157,15 +124,13 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         auto *_t = static_cast<MainWindow *>(_o);
         (void)_t;
         switch (_id) {
-        case 0: _t->timerEvent((*reinterpret_cast< std::add_pointer_t<QTimerEvent*>>(_a[1]))); break;
-        case 1: _t->getData(); break;
-        case 2: _t->setIpProd(); break;
-        case 3: _t->showProd(); break;
-        case 4: _t->startTemp(); break;
-        case 5: _t->stopTemp(); break;
+        case 0: _t->getData(); break;
+        case 1: _t->startTemp(); break;
+        case 2: _t->stopTemp(); break;
         default: ;
         }
     }
+    (void)_a;
 }
 
 const QMetaObject *MainWindow::metaObject() const
@@ -187,13 +152,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 6)
+        if (_id < 3)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 6;
+        _id -= 3;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 6)
+        if (_id < 3)
             *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
-        _id -= 6;
+        _id -= 3;
     }
     return _id;
 }
